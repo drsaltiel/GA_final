30835fefcbb5b76c4418d98a9ed5cd5f13c62720
Daniel <drsaltiel@gmail.com>
2014-12-13 18:31:45 -0800

    added suggest_by_location initial commit

---
+++ suggest_by_location.py
@@ -0,0 +1,113 @@
+'''
+Returns the optimal business for a given location based upon surrounding businesses
+within a specified radius.
+
+Needs input of latitude and longitude, defaults:
+radius = 1 mile
+metric = maximum traffic (reviews) percent
+possible_categories = all possible categories
+num_results = results for all categories
+city = Las Vegas
+'''
+
+import pandas as pd
+import ast
+import numpy as np
+import math as math
+import sys
+
+
+def find_optimal_bus(latitude, longitude, radius, metric, 
+                     possible_categories=None, num_results=None, city='Las Vegas'):
+    '''
+    takes in a coordinate location and finds what category of business 
+    from possible_categories would be best there according to metric
+    based on information about businesses within radius
+    '''
+    bus_data=pd.read_csv('yelp_academic_dataset_business.csv')
+    city_data = bus_data[bus_data['city']== city]
+    if possible_categories is None:
+        possible_categories = []
+        for i, j in city_data['categories'].iteritems():
+            possible_categories+=ast.literal_eval(j)
+        possible_categories=list(set(possible_categories))
+    if num_results==None:
+        num_results = len(possible_categories)
+    
+    results = [(cat, metric(latitude, longitude,cat, radius, city_data)) \
+               for cat in possible_categories]
+    results.sort(key=lambda pair: pair[1], reverse=True)
+        
+    return results[:num_results]
+    
+def max_traffic_percent(lat,lon,category,radius, city):
+    '''
+    metric of maximum percent of traffic
+    '''
+    cat_rev, total_rev = review_count_radius(lat, lon, category, radius, city)
+    cat_percent = float(cat_rev) / total_rev
+    return cat_percent
+
+def min_traffic_percent(lat,lon,category,radius, city):
+    '''
+    metric of minimum percent of traffic
+    '''
+    cat_rev, total_rev = review_count_radius(lat, lon, category, radius, city)
+    cat_percent = float(cat_rev) / total_rev
+    return -cat_percent
+
+def review_count_radius(lat, lon, category, radius, city):
+    '''
+    returns category businesses and total businesses within radius in miles
+    '''
+    lat_bus = lat
+    lon_bus = lon
+    category_reviews = 0
+    total_reviews = 0
+    for i, bus in city['categories'].iteritems():
+        lat = city['latitude'][i]
+        lon = city['longitude'][i]
+        
+        if radius < coord_distance((lon_bus,lat_bus),(lon,lat)):
+            total_reviews  +=city['review_count'][i]
+            if category in bus:
+                category_reviews+=city['review_count'][i]
+    
+    return category_reviews, total_reviews
+
+def coord_distance(point1, point2, scale='miles'):
+    '''
+    finds the distance between two coordinate locations given as
+    (longitude, latitude)
+    uses Haversine formula
+    '''
+    if len(point1) != 2 or len(point2) != 2:
+        raise ValueError('Input(s) do not have correct dimensions')
+    if scale == 'miles': 
+        R = 3963
+    elif scale == 'kilometers': 
+        R = 6373
+    else: 
+        raise ValueError('Need valid scale (miles or kilometers)')
+    dLat = (point1[1] - point2[1]) * np.pi / 180
+    dLong = (point1[0] - point2[0]) * np.pi / 180
+    a = np.sin(dLat / 2) * np.sin(dLat / 2) + np.cos(point1[1] * np.pi / 180) \
+        * np.cos(point2[1] * np.pi / 180) * np.sin(dLong / 2) * np.sin(dLong / 2)
+    c = 2 * math.atan2(np.sqrt(a), np.sqrt(1 - a))
+    return R*c
+
+def main(latitude, longitude):
+    suggestions = find_optimal_bus(latitude, 
+                           longitude, 
+                           radius = 1, 
+                           metric = max_traffic_percent, 
+                           possible_categories=None, 
+                           num_results=None, 
+                           city='Las Vegas')
+    return suggestions
+
+
+if __name__ == '__main__':
+    lat = sys.argv[1]
+    lon = sys.argv[2]
+    main(lat, lon)
